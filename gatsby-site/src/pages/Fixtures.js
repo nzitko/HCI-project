04d2cf538204ad1431db8592fe7c8a92eca52376
PetarOlujic <petar.olujic4@gmail.com>
2020-01-08 13:25:03 +0100

    test

--- gatsby-site/src/pages/Fixtures.js
+++ gatsby-site/src/pages/Fixtures.js
@@ -1,17 +1,29 @@
 import React from "react"
 //import { Link } from "gatsby"
 //import Navigation from "../components/navigation"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import logo from "../images/everton-logo.png"
+import logo1 from "../images/west-ham-logo.png"
 
-const fixturesPage = () => (  
-<div>
+const fixturesPage = () => (
   <Layout>
     <SEO title="Fixtures" />
+    <p className="page-title">Fixtures</p>
+    <div className="fixtures-title-name">Gameweek 10</div>
+    <div className="fixure-block">
+      <div className="fixture-info">
+        Goodison Park <br /> SAT 12:30 CET
+      </div>
+
+      <div className="fixture-icons">
+        <img src={logo} alt="everton"></img>
+        <img src={logo1} alt="west-ham"></img>
+      </div>
+
+      <div className="fixture-result"></div>
+    </div>
   </Layout>
-  <p className = "page-title">Fixtures</p>
-</div>
 )
 
 export default fixturesPage
--- gatsby-site/src/pages/Log-in.js
+++ gatsby-site/src/pages/Log-in.js
@@ -3,18 +3,19 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const logInPage = () => (
-<div>
+
   <Layout>
     <SEO title="Log in" />
+    <div className = "log-in-container">
+      <p className="page-title">Log in</p>
+      <input type="text" placeholder="Username" className="inputField"></input>
+      <input type="text" placeholder="E-mail" className="inputField"></input>
+      <input type="password" placeholder="Password" className="inputField"></input>
+      <input type="password" placeholder="Repeat password" className="inputField"></input>
+      <button className="btn">Log in</button>
+    </div>
   </Layout>
-  <div className = "log-in-container">
-   <p className="page-title">Log in</p>
-   <input type="text" placeholder="Username" className="inputField"></input>
-   <input type="password" placeholder="Password" className="inputField"></input>
-   <input type="password" placeholder="Repeat password" className="inputField"></input>
-   <button className="btn">Log in</button>
-  </div>
-</div>
+ 
 )
 
 export default logInPage
