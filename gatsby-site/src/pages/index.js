41d3bdee69c07720f166b56e92abd71e3c84da80
nzitko <nikola.ztk@gmail.com>
2020-02-15 18:13:12 +0100

    added carousel

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,18 +1,58 @@
 import React from "react"
 //import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
+import ars from "../images/emirates.jpg"
+import avl from "../images/Aston-Villa-stadium.jpg"
+
+const photos = [
+
+  {
+      name:"arsenal",
+      src:ars
+  },
+
+  {
+    name:"aston villa",
+    src:avl
+  }
+]
 
 
-const IndexPage = () => (
-  <div>
+class IndexPage extends React.Component{
+  render(){
+    const settings = {
+      dots:false,
+      fade:true,
+      infinite:true,
+      speed:750,
+      slidesToShow:1,
+      arrows:true,
+      slidesToScroll:1,
+      className:"slides"
+    }
+  return(
     <Layout>
-      <SEO title="Home" />
+      <SEO title="Home"/>
+      <p className = "page-title">Home</p>
+      <Slider {...settings}> 
+        {photos.map((photo)=>{
+          return(
+            <div className="slider-container">
+              <img width="100%" src={photo.src} className="slider-picture"/>
+            </div>
+          )
+        })}
+
+      </Slider>
     </Layout>
-    <p className = "page-title">Home</p>
-  </div>
-)// sve sta wrappas u Layout se stavi u objekt koji ima svoj propety s nazivom CHILDREN u koji se to sve pohrani
+  );
+ }
+}
+
 
 export default IndexPage
